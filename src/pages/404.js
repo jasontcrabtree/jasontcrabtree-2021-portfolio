03109354bd562f81e8a449f9d83a0fe2213c2e39
jasontcrabtree <jasontcrabtree@gmail.com>
2020-11-30 21:10:18 +1300

    add 404 page component

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PageNotFound = (props) => {
+  console.log(props);
+
+  return (
+    <>
+      <h1>404 Page</h1>
+      <p>Missing page</p>
+    </>
+  );
+};
+
+export default PageNotFound;
